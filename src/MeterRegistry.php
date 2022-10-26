d08bc740be52183d3534d678e3a889c8b7659e11
Yurun <admin@yurunsoft.com>
2022-10-26 11:32:40 +0800

    回滚：新增上下文销毁时自动关闭

--- src/MeterRegistry.php
+++ src/MeterRegistry.php
@@ -6,25 +6,18 @@ namespace Imi\Meter;
 
 use Imi\App;
 use Imi\Bean\Annotation\Bean;
-use Imi\Event\Event;
 use Imi\Meter\Contract\IMeterRegistry;
-use Imi\RequestContext;
 
 /**
  * @Bean("MeterRegistry")
  */
 class MeterRegistry
 {
-    public const CONTEXT_KEY = self::class . '.driverInstance';
-
     protected string $driver = '';
 
     protected array $options = [];
 
-    public function __construct()
-    {
-        Event::on('IMI.REQUEST_CONTENT.DESTROY', fn () => $this->onContextDestroy());
-    }
+    private ?IMeterRegistry $driverInstance = null;
 
     public function getDriver(): string
     {
@@ -38,29 +31,16 @@ class MeterRegistry
 
     public function getDriverInstance(): IMeterRegistry
     {
-        $context = RequestContext::getContext();
-        if (isset($context[self::CONTEXT_KEY]))
+        if (!$this->driverInstance)
         {
-            return $context[self::CONTEXT_KEY];
+            if ('' === $this->driver)
+            {
+                throw new \InvalidArgumentException('Config @app.beans.MeterRegistry.driver cannot be empty');
+            }
+            // @phpstan-ignore-next-line
+            return $this->driverInstance = App::getBean($this->driver, $this->options);
         }
-        if ('' === $this->driver)
-        {
-            throw new \InvalidArgumentException('Config @app.beans.MeterRegistry.driver cannot be empty');
-        }
-        /** @var IMeterRegistry $instance */
-        // @phpstan-ignore-next-line
-        $instance = App::newInstance($this->driver, $this->options);
-        RequestContext::set(self::CONTEXT_KEY, $instance);
 
-        return $instance;
-    }
-
-    protected function onContextDestroy(): void
-    {
-        if ($instance = RequestContext::get(self::CONTEXT_KEY))
-        {
-            /** @var IMeterRegistry $instance */
-            $instance->close();
-        }
+        return $this->driverInstance;
     }
 }
