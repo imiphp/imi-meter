4ef1a079154f636bba4deadfb4f851e5c4d1922e
Yurun <admin@yurunsoft.com>
2022-10-26 13:25:06 +0800

    优化：在适当的时候关闭 MeterRegistry

--- src/MeterRegistry.php
+++ src/MeterRegistry.php
@@ -6,6 +6,7 @@ namespace Imi\Meter;
 
 use Imi\App;
 use Imi\Bean\Annotation\Bean;
+use Imi\Event\Event;
 use Imi\Meter\Contract\IMeterRegistry;
 
 /**
@@ -37,10 +38,21 @@ class MeterRegistry
             {
                 throw new \InvalidArgumentException('Config @app.beans.MeterRegistry.driver cannot be empty');
             }
+            Event::on(['IMI.MAIN_SERVER.WORKER.EXIT', 'IMI.MAIN_SERVER.WORKER.STOP', 'IMI.PROCESS.END'], fn () => $this->onShutdown(), \Imi\Util\ImiPriority::IMI_MIN);
+            register_shutdown_function(fn () => $this->onShutdown());
             // @phpstan-ignore-next-line
             return $this->driverInstance = App::getBean($this->driver, $this->options);
         }
 
         return $this->driverInstance;
+    }
+
+    protected function onShutdown(): void
+    {
+        if ($instance = $this->driverInstance)
+        {
+            /** @var IMeterRegistry $instance */
+            $instance->close();
+        }
     }
 }
